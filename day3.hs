0f77d5062ebaeef9a38594060894d5bfb483d66d
Pavel Skripkin <paskripkin@gmail.com>
2024-12-04 00:30:00 +0300

    day3 part 2

--- day3.hs
+++ day3.hs
@@ -9,20 +9,25 @@ parseInt (x : xs) =
       helper [] acc = (acc, [])
    in if isNumber x then Just $ helper (x : xs) 0 else Nothing
 
-parse :: String -> Int -> Int
-parse [] acc = acc
-parse (x : xs) acc = case x : xs of
+parse :: String -> Int -> Bool -> Int
+parse [] acc en = acc
+parse list@(x : xs) acc en = case list of
+  ('d' : 'o' : 'n' : '\'' : 't' : '(' : ')' : others) -> parse others acc False
+  ('d' : 'o' : '(' : ')' : others) -> parse others acc True
   ('m' : 'u' : 'l' : '(' : others) ->
-    fromMaybe
-      (parse others acc)
-      ( do
-          (val, x1 : xs1) <- parseInt others
-          (val1, x2 : xs2) <- if x1 == ',' then parseInt xs1 else Nothing
-          (val2, xs3) <- if x2 == ')' then Just (val1, xs2) else Nothing
-          return $ parse xs2 (acc + (val2 * val))
-      )
-  (x : xs) -> parse xs acc
+    if en
+      then
+        fromMaybe
+          (parse others acc en)
+          ( do
+              (val, x1 : xs1) <- parseInt others
+              (val1, x2 : xs2) <- if x1 == ',' then parseInt xs1 else Nothing
+              (val2, xs3) <- if x2 == ')' then Just (val1, xs2) else Nothing
+              return $ parse xs2 (acc + (val2 * val)) en
+          )
+      else parse others acc en
+  _ -> parse xs acc en
 
-main1 = do
+main = do
   s <- readFile "input"
-  print $ parse s 0
+  print $ parse s 0 True
